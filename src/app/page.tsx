774358f9a2cef8ab07b64a4b10f95ca30382ab25
Bernardo-Mattos <bernardomtt2@gmail.com>
2024-02-27 11:25:26 -0300

    feat: add table on screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,36 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>CRUD com tauri</h1>
-      
-    </main>
+    <div className="flex max-h-screen flex-col items-center justify-between p-24 min-w-max">
+      <div className="border rounded">
+        <h1>CRUD com tauri</h1>
+        <Table>
+          <TableHeader>
+            <TableHead>Id</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Age</TableHead>
+            <TableHead>Actions</TableHead>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>
+                1
+              </TableCell>
+              <TableCell>
+                Bernardo
+              </TableCell>
+              <TableCell>
+                18
+              </TableCell>
+              <TableCell>
+              <Button variant="destructive">Delete</Button>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,7 @@
 import type { Config } from "tailwindcss"
+const { fontFamily } = require("tailwindcss/defaultTheme")
+
+/** @type {import('tailwindcss').Config} */
 
 const config = {
   darkMode: ["class"],
@@ -7,7 +10,7 @@ const config = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
+  ],
   prefix: "",
   theme: {
     container: {
@@ -71,6 +74,9 @@ const config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
       },
     },
   },
